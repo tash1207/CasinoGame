3b54ce6ed8389179ca1b74b8876c8c38caa0441e
Tasha Hankewych <thankewych@gmail.com>
2026-09-17 20:55:24 -0500

    Add sprites for each card suit

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -1,10 +1,16 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
     [SerializeField] TMP_Text cardValueText;
-    [SerializeField] TMP_Text cardSuitText;
+    [SerializeField] Image cardSuitImage;
+
+    [SerializeField] Sprite spadeSprite;
+    [SerializeField] Sprite heartSprite;
+    [SerializeField] Sprite clubSprite;
+    [SerializeField] Sprite diamondSprite;
 
     public void SetCard(Card card)
     {
@@ -13,27 +19,25 @@ public class CardDisplay : MonoBehaviour
         {
             case Card.Suit.SPADE:
                 suitColor = Color.black;
-                cardSuitText.text = "S";
+                cardSuitImage.sprite = spadeSprite;
                 break;
             case Card.Suit.HEART:
                 suitColor = Color.red;
-                cardSuitText.text = "H";
+                cardSuitImage.sprite = heartSprite;
                 break;
             case Card.Suit.CLUB:
                 suitColor = Color.black;
-                cardSuitText.text = "C";
+                cardSuitImage.sprite = clubSprite;
                 break;
             case Card.Suit.DIAMOND:
                 suitColor = Color.red;
-                cardSuitText.text = "D";
+                cardSuitImage.sprite = diamondSprite;
                 break;
             default:
                 return;
         }
 
         cardValueText.text = card.valueName;
-
-        cardSuitText.color = suitColor;
         cardValueText.color = suitColor;
     }
 }
--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 
 public class CardManager : MonoBehaviour
 {
