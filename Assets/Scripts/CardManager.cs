41c63dec1b319c676320cc2066b0311d0c531f9b
Tasha Hankewych <thankewych@gmail.com>
2026-09-22 01:15:17 -0500

    Highlight dealer's cards when they win and your cards when you win (and your cards when you tie)

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -27,6 +27,9 @@ public class CardManager : MonoBehaviour
     private List<Card> tableCards;
     private List<Card> dealerCards;
 
+    private Hand yourHand;
+    private Hand dealersHand;
+
     private List<GameObject> tableCardDisplays;
 
     void OnEnable()
@@ -92,8 +95,8 @@ public class CardManager : MonoBehaviour
 
         handCard1.transform.parent.gameObject.SetActive(true);
         handCard2.transform.parent.gameObject.SetActive(true);
-        dealerCard1.gameObject.SetActive(true);
-        dealerCard2.gameObject.SetActive(true);
+        dealerCard1.transform.parent.gameObject.SetActive(true);
+        dealerCard2.transform.parent.gameObject.SetActive(true);
 
         handCards.Add(randomCard1);
         handCard1.SetCard(randomCard1);
@@ -138,8 +141,23 @@ public class CardManager : MonoBehaviour
 
     public void ShowHand()
     {
-        Hand yourHand = HandManager.GetHand(handCards, tableCards);
-        Hand dealersHand = HandManager.GetHand(dealerCards, tableCards);
+        yourHand = HandManager.GetHand(handCards, tableCards);
+        dealersHand = HandManager.GetHand(dealerCards, tableCards);
+
+        if (yourHand.score > dealersHand.score)
+        {
+            HighlightYourCards();
+            Debug.Log("You won!");
+        }
+        else if (yourHand.score == dealersHand.score)
+        {
+            HighlightYourCards();
+            Debug.Log("TIE");
+        } else
+        {
+            HighlightDealersCards();
+            Debug.Log("You lose");
+        }
 
         showHandText.text = "Your Hand:\n" + yourHand.handText;
         showDealerHandText.text = "Dealer's Hand:\n" + dealersHand.handText;
@@ -147,18 +165,19 @@ public class CardManager : MonoBehaviour
 
         dealerCard1.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
         dealerCard2.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-
-        HighlightYourCards(yourHand);
     }
 
-    void HighlightYourCards(Hand yourHand)
+    public void HighlightYourCards()
     {
+        ResetHighlightedCards();
         foreach (var tableCardDisplay in tableCardDisplays)
         {
             Card tableCard = tableCardDisplay.GetComponentInChildren<MeshCardDisplay>().GetCard();
             if (yourHand.handCards.Contains(tableCard))
             {
-                tableCardDisplay.GetComponent<Outline>().enabled = true;
+                Outline outline = tableCardDisplay.GetComponent<Outline>();
+                outline.enabled = true;
+                outline.effectColor = new Color(1f, 0f, 1f, 1f);
             }
         }
 
@@ -170,6 +189,42 @@ public class CardManager : MonoBehaviour
         {
             handCard2.gameObject.GetComponentInParent<Outline>().enabled = true;
         }
+    }
+
+    public void HighlightDealersCards()
+    {
+        ResetHighlightedCards();
+        foreach (var tableCardDisplay in tableCardDisplays)
+        {
+            Card tableCard = tableCardDisplay.GetComponentInChildren<MeshCardDisplay>().GetCard();
+            if (dealersHand.handCards.Contains(tableCard))
+            {
+                Outline outline = tableCardDisplay.GetComponent<Outline>();
+                outline.enabled = true;
+                outline.effectColor = new Color(1f, 1f, 0f, 1f);
+            }
+        }
+
+        if (dealersHand.handCards.Contains(dealerCard1.GetCard()))
+        {
+            dealerCard1.gameObject.GetComponentInParent<Outline>().enabled = true;
+        }
+        if (dealersHand.handCards.Contains(dealerCard2.GetCard()))
+        {
+            dealerCard2.gameObject.GetComponentInParent<Outline>().enabled = true;
+        }
+    }
+
+    void ResetHighlightedCards()
+    {
+        foreach (var tableCardDisplay in tableCardDisplays)
+        {
+            tableCardDisplay.GetComponent<Outline>().enabled = false;
+        }
+        handCard1.gameObject.GetComponentInParent<Outline>().enabled = false;
+        handCard2.gameObject.GetComponentInParent<Outline>().enabled = false;
+        dealerCard1.gameObject.GetComponentInParent<Outline>().enabled = false;
+        dealerCard2.gameObject.GetComponentInParent<Outline>().enabled = false;
     }
 
     public void Reset()
@@ -188,8 +243,11 @@ public class CardManager : MonoBehaviour
 
         dealerCard1.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
         dealerCard2.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-        dealerCard1.gameObject.SetActive(false);
-        dealerCard2.gameObject.SetActive(false);
+        dealerCard1.gameObject.GetComponentInParent<Outline>().enabled = false;
+        dealerCard2.gameObject.GetComponentInParent<Outline>().enabled = false;
+
+        dealerCard1.transform.parent.gameObject.SetActive(false);
+        dealerCard2.transform.parent.gameObject.SetActive(false);
 
         List<GameObject> cardsToDestroy = new List<GameObject>();
         foreach (Transform child in cardHolder.transform)
